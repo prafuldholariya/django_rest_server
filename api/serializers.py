7eac4b5f430622f645b1bbc557ac6fc0f142f7d9
Praful Dholariya <praful.dholariya@devputers.com>
2024-10-02 09:21:18 +0530

    Add user serializers for registration and login

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'phone_number', 'address']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # Hash password
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
